f4d24b6d6589835f47ed88f94527a99213e77e67
Berenger <berenger.benoit@gmail.com>
2020-03-10 15:02:15 +0100

    Database and entity

---
+++ src/Entity/Pokemon.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PokemonRepository")
+ */
+class Pokemon
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $pv;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $attaque;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $defense;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $att_spec;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $def_spec;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $vitesse;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Type", inversedBy="pokemon")
+     */
+    private $type;
+
+    public function __construct()
+    {
+        $this->type = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPv(): ?int
+    {
+        return $this->pv;
+    }
+
+    public function setPv(int $pv): self
+    {
+        $this->pv = $pv;
+
+        return $this;
+    }
+
+    public function getAttaque(): ?int
+    {
+        return $this->attaque;
+    }
+
+    public function setAttaque(int $attaque): self
+    {
+        $this->attaque = $attaque;
+
+        return $this;
+    }
+
+    public function getDefense(): ?int
+    {
+        return $this->defense;
+    }
+
+    public function setDefense(int $defense): self
+    {
+        $this->defense = $defense;
+
+        return $this;
+    }
+
+    public function getAttSpec(): ?int
+    {
+        return $this->att_spec;
+    }
+
+    public function setAttSpec(int $att_spec): self
+    {
+        $this->att_spec = $att_spec;
+
+        return $this;
+    }
+
+    public function getDefSpec(): ?int
+    {
+        return $this->def_spec;
+    }
+
+    public function setDefSpec(int $def_spec): self
+    {
+        $this->def_spec = $def_spec;
+
+        return $this;
+    }
+
+    public function getVitesse(): ?int
+    {
+        return $this->vitesse;
+    }
+
+    public function setVitesse(int $vitesse): self
+    {
+        $this->vitesse = $vitesse;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Type[]
+     */
+    public function getType(): Collection
+    {
+        return $this->type;
+    }
+
+    public function addType(Type $type): self
+    {
+        if (!$this->type->contains($type)) {
+            $this->type[] = $type;
+        }
+
+        return $this;
+    }
+
+    public function removeType(Type $type): self
+    {
+        if ($this->type->contains($type)) {
+            $this->type->removeElement($type);
+        }
+
+        return $this;
+    }
+}
