0f0addc048cf618a6d40fabff0769a7b9b6d3ebe
Berenger <berenger.benoit@gmail.com>
2020-03-10 22:36:55 +0100

    display card pokemon

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Pokemon;
+use App\Repository\PokemonRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(PokemonRepository $pr)
     {
-        return $this->render('home/index.html.twig');
+        $pokemons = $pr->findAll();
+        return $this->render('home/index.html.twig', [
+            'pokemons' => $pokemons
+        ]);
     }
 }
